a25d3bc211e086ce8c6695eff4998b600c698bc6
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2024-06-02 16:11:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "29922662"))
+API_HASH = getenv("API_HASH", "fabd9f89368de7cc31357522a0089a56")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "7209509711:AAFgCugkXL7NgDt__CFvFsPpJyK_jd4eEsU")
 #OWNER_ID = int(getenv("6752444356"))
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+OWNER_ID = int(getenv("OWNER_ID", "6047510747"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://animebot:animebot@cluster0.h0ix44k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MUST_JOIN = getenv("MUST_JOIN", "Cv_official_channel")
